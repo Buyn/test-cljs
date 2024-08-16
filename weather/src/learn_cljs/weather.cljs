b06fa9cd226e7e5e8efa83c13e07f15425af60f6
buyn <buyn.tex@gmail.com>
2024-08-16 20:20:15 +0300

    retanglet all

--- weather/src/learn_cljs/weather.cljs
+++ weather/src/learn_cljs/weather.cljs
@@ -4,12 +4,41 @@
    [reagent.dom :as rdom]
    [reagent.core :as r]))
 
-(defn hello-world []                                       ;; <2>
-  [:div
-   [:h1 {:class "app-title"} "Hello, World"]])
+(defonce app-state (r/atom {:title "WhichWeather"
+                            :postal-code ""
+                            :temperatures {:today {:label "Today"
+                                                   :value nil}
+                                           :tomorrow {:label "Tomorrow"
+                                                      :value nil}}}))
 
-(defn mount-app-element []                                 ;; <3>
-  (rdom/render [hello-world] (gdom/getElement "app")))
+(defn title []
+  [:h1 (:title @app-state)])
+
+(defn temperature [temp]                                   ;; <1>
+  [:div {:class "temperature"}
+   [:div {:class "value"}
+    (:value temp)]
+   [:h2 (:label temp)]])
+
+(defn postal-code []
+  [:div {:class "postal-code"}
+   [:h3 "Enter your postal code"]
+   [:input {:type "text"
+            :placeholder "Postal Code"
+            :value (:postal-code @app-state)}]
+   [:button "Go"]])
+
+(defn app []
+  [:div {:class "app"}
+   [title]                                                 ;; <2>
+   [:div {:class "temperatures"}
+    (for [temp (vals (:temperatures @app-state))]          ;; <3>
+      [temperature temp])]
+   [postal-code]])
+
+(defn mount-app-element []                                 ;; <4>
+  (rdom/render [app] (gdom/getElement "app")))
+
 (mount-app-element)
 
 (defn ^:after-load on-reload []                            ;; <4>
