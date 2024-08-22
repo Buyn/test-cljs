9654579a217d3d2425d7baf63bd5ee6005ba1e0e
buyn <buyn.tex@gmail.com>
2024-08-22 18:20:50 +0300

    add promt fun

--- starbase/src/learn_cljs/starbase.cljs
+++ starbase/src/learn_cljs/starbase.cljs
@@ -11,9 +11,23 @@
                 {:prompt "=> "
                  :font-size 14}))
 
+
+
 (declare on-answer)
 
-(defn prompt [game current])
+(defn prompt [game current]                                ;; <1>
+  (let [scene (get game current)                           ;; <2>
+        type (:type scene)]
+    (io/clear term)
+    (when (or (= :win type)                                ;; <3>
+              (= :lose type))
+      (io/print term
+                (if (= :win type)                          ;; <4>
+                    "You've Won! "
+                    "Game Over ")))
+    (io/println term (:title scene))                       ;; <5>
+    (io/println term (:dialog scene))
+    (io/read term #(on-answer game current %))))           ;; <6>
 
 (defn on-answer [game current answer])
 
