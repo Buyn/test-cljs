c7b418c9367e2cbfec4b27a52788347020d241a8
buyn <buyn.tex@gmail.com>
2024-11-24 17:47:15 +0200

    srart editing

--- contacts/src/learn_cljs/contacts.cljs
+++ contacts/src/learn_cljs/contacts.cljs
@@ -1,22 +1,2 @@
-(ns ^:figwheel-hooks learn-cljs.contacts
-  (:require
-   [goog.dom :as gdom]))
-
-(println "This text is printed from src/learn_cljs/contacts.cljs. Go ahead and edit it and see reloading in action.")
-
-(defn multiply [a b] (* a b))
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
-
-
-
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn make-contact [contact]
+  (select-keys contact [:first-name :last-name :email :postal :address]))
---
+++ doing-io/src/learn_cljs/doing-io.cljs
@@ -0,0 +1,22 @@
+(ns ^:figwheel-hooks learn-cljs.doing-io
+  (:require
+   [goog.dom :as gdom]))
+
+(println "This text is printed from src/learn_cljs/doing_io.cljs. Go ahead and edit it and see reloading in action.")
+
+(defn multiply [a b] (* a b))
+
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+
+
+
+(defn ^:after-load on-reload []
+
+
+
+)
