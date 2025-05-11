4d43c219d5c7968da940ceb3269cdab9bdd29843
buyn <buyn.tex@gmail.com>
2025-05-11 18:24:03 +0300

    start transfer all from book version

--- reagent-test/src/learn_cljs/reagent_test.cljs
+++ reagent-test/src/learn_cljs/reagent_test.cljs
@@ -32,11 +32,6 @@
 (defn hello []
   [:p "Hello World"])
 
-(defn date-input []
-  [:div.input-wrapper                                      ;; <1>
-    [:label "Day"]
-    [:input {:type "date"}]])                              ;; <2>
-
 (defn time-input []
   [:div.input-wrapper
     [:label "Time (minutes)"]
@@ -47,10 +42,45 @@
     [:button {:type "submit"} "Submit"]])
 
 (defn form []
-  [:form.input-form
+  [:form.input-form {:on-submit (fn [e]
+                                    (.preventDefault e)
+                                    (swap! state submit-form))}
     [date-input]                                           ;; <3>
     [time-input]
     [submit-button]])
+
+(defn initial-inputs []
+
+  {:date (current-date-string (js/Date.))
+   :minutes "0"})
+
+(defonce state
+  (r/atom {:inputs (initial-inputs)
+           :entries {}}))
+
+(defn submit-form [state]
+  (let [{:keys [date minutes]} (:inputs state)]
+    (-> state
+        (assoc-in [:entries date] (js/parseInt minutes))
+        (assoc :inputs (initial-inputs)))))
+
+(defn- current-date-string [d]
+  (let [pad-zero #(.padStart (.toString %) 2 "0")
+        y (.getFullYear d)
+        m (-> (.getMonth d) inc pad-zero)
+        d (pad-zero (.getDate d))]
+    (str y "-" m "-" d)))
+
+
+(defn date-input []
+  (let [val (r/cursor state [:inputs :date])]              ;; <1>
+    (fn []
+      [:div.input-wrapper
+        [:label "Day"]
+        [:input {:type "date"
+                 :value @val                               ;; <2>
+                 :on-change #(reset! val                   ;; <3>
+                               (.. % -target -value))}]])))
 
 (defn- random-point []
   (js/Math.floor (* (js/Math.random) 100)))
@@ -65,20 +95,39 @@
 (def bar-spacing 2)
 
 (defn chart []
-  (let [{:keys [points chart-max]} @chart-data             ;; <2>
-        bar-width (- (/ chart-width (count points))
-                     bar-spacing)]
-    [:svg.chart {:x 0 :y 0
-                 :width chart-width :height chart-height}
-      (for [[i point] (map-indexed vector points)          ;; <3>
-            :let [x (* i (+ bar-width bar-spacing))        ;; <4>
-                  pct (- 1 (/ point chart-max))
-                  bar-height (- chart-height (* chart-height pct))
-                  y (- chart-height bar-height)]]
-        [:rect {:key i                                     ;; <5>
-                :x x :y y
-                :width bar-width
-                :height bar-height}])]))
+
+  (let [entries (r/cursor state [:entries])                ;; <1>
+        chart-data (ratom/make-reaction                    ;; <2>
+          #(let [points (get-points @entries)]
+                { :points points
+                  :chart-max (reduce max 1 points)})
+              bar-width (- (/ chart-width (count points))
+                          bar-spacing))]
+    (fn []
+      (let [{:keys [points chart-max]} @chart-data
+            bar-width (- (/ chart-width (count points))
+                         bar-spacing)]
+        [:svg.chart {:x 0 :y 0
+                     :width chart-width :height chart-height}
+         (for [[i point] (map-indexed vector points)
+               :let [x (* i (+ bar-width bar-spacing))
+                     pct (- 1 (/ point chart-max))
+                     bar-height (- chart-height (* chart-height pct))
+                     y (- chart-height bar-height)]]
+           [:rect {:key i
+                   :x x :y y
+                   :width bar-width
+                   :height bar-height}])]))))
+
+(defn get-points [entries]
+  (let [ms-in-day 86400000
+        chart-days 30
+        now (js/Date.now)]
+    (map (fn [i]
+           (let [days-ago (- chart-days (inc i))
+                 date (date-string (js/Date. (- now (* ms-in-day days-ago))))]
+             (get entries date 0)))
+         (range chart-days))))
 
 (defn app []
   [:div.app
