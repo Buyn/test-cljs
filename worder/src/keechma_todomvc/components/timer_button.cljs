70e9492965d3f385081f0992a8f780ea9e2b18b1
buyn <buyn.tex@gmail.com>
2025-11-06 16:11:50 +0200

    add running? fn and color on running

--- worder/src/keechma_todomvc/components/timer_button.cljs
+++ worder/src/keechma_todomvc/components/timer_button.cljs
@@ -6,6 +6,7 @@
   (let [seconds (r/atom 0)
         timer-id (r/atom nil)
         start-ms (r/atom nil)
+        running? (fn [](not (= @timer-id nil)))
         counter (fn [] (reset! seconds (/ (- (.now js/Date) @start-ms) 1000)))
         stop! (fn []
                 (counter)
@@ -15,12 +16,12 @@
         start! (fn []
                   (reset! start-ms (.now js/Date))
                   (reset! timer-id (js/setInterval counter 100)))
-        toggle! (fn [_] (if @timer-id (stop!) (start!)))]  
+        toggle! (fn [_] (if (running?) (stop!) (start!)))]  
     (fn []
-      [:div.timer
+      [:div.timer {:class (when (running?) "running")}
        [:button
         {:on-click toggle!} 
-        (if @timer-id "■ Stop" "▶ Start")]
+        (if (running?) "■ Stop" "▶ Start")]
        [:span.seconds (str @seconds " sec")]])))
 
 (def component
