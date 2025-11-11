4edae688bb5443c4cd4257225cc96a570514bdf1
buyn <buyn.tex@gmail.com>
2025-11-11 11:14:31 +0200

    fix timer button position

--- worder/src/keechma_todomvc/components/todo_item.cljs
+++ worder/src/keechma_todomvc/components/todo_item.cljs
@@ -32,8 +32,8 @@ If the item is currently being edited, overlays a `todo-edit` component.
     [:input.toggle {:type :checkbox
                     :checked (:completed todo)
                     :on-change #(<cmd ctx :toggle-todo todo)}]
-    [:label (:title todo)
-      [comp> ctx :timer-button todo]]
+    [:label (:title todo)]
+    [comp> ctx :timer-button todo]
     [:button.destroy {:on-click #(<cmd ctx :delete-todo todo)}]]
    (when is-editing?
      [comp> ctx :todo-edit])])
