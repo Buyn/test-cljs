89672bbedb4f71c70fa93cd3321c550b1b9d103a
buyn <buyn.tex@gmail.com>
2025-11-19 07:57:23 +0200

    end gigs component

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -5,10 +5,13 @@
   [gig]
   [:div.gig {:key (:id gig)}
     [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
-    [:div.gig
-      [:div.title (:title gig)]
-      [:div.desc (:desc gig)]]
-   ])
+    [:div.gig__body
+     [:div.gig__title
+      [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
+       [:i.icon.icon--plus]]
+      (:title gig)]]
+   [:p.gig__price (:price gig)]
+   [:p.gig__desc (:desc gig)]])
 
 (defn gigs
   []
