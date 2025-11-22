1b32c236dde085889a6445c7fb60bcb336080993
buyn <buyn.tex@gmail.com>
2025-11-22 06:22:59 +0200

    add modal gig edit

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,25 +1,37 @@
 (ns giggin.components.gigs
   (:require [giggin.state :as state ]
+            [giggin.components.gig-editor :refer [gig-editor]]
+            [reagent.core :as r]
             [giggin.helpers :refer [format-price]]))
 
-(defn gig
-  [{:keys [id img title price desc] :as el}]
+(defn gig [{:keys [id img title price desc] :as el}]
     (let [add-to-order #(swap! state/orders update id inc)]
-    [:div.gig {:key id}
-      [:img.gig__artwork {:src img :alt title}]
-      [:div.gig__body
-        [:div.gig__title
-          [:div.btn.btn--primary.float--right.tooltip
-            { :data-tooltip "Add to order!"
-              :on-click add-to-order}
-            [:i.icon.icon--plus]]
-          title]]
-      [:p.gig__price (format-price price)]
-      [:p.gig__desc desc]]))
+      [:div.gig {:key id}
+        [:img.gig__artwork {:src img :alt title}]
+        [:div.gig__body
+          [:div.gig__title
+            [:div.btn.btn--primary.float--right.tooltip
+              { :data-tooltip "Add to order!"
+                :on-click add-to-order}
+              [:i.icon.icon--plus]]
+            title]]
+        [:p.gig__price (format-price price)]
+        [:p.gig__desc desc]]))
 
-(defn gigs
-  []
-  [:main
-    [:div.gigs
-     (for [el (vals @state/gigs)]
-       (gig el))]])
+(defn btn-add-gig []
+  (let [modal (r/atom false)
+        values (r/atom {:id nil :title "" :desc "" :price 0 :img "" :sold-out false})]
+        [:div.gig
+          [:button.add-gig
+              {:on-click #(reset! modal true)}
+              [:div.add__title
+            [:i.icon.icon--plus]
+            [:h1 "Add gig"]]]
+          [gig-editor modal values]]))
+
+(defn gigs []
+    [:main
+      [:div.gigs
+        [btn-add-gig]
+        (for [el (vals @state/gigs)]
+          (gig el))]])
